fd0f60c84d744e199a8cb19515636d521bf5c06b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 16:05:02 -0400

    updated

--- antigen_discrepancy.py
+++ antigen_discrepancy.py
@@ -31,9 +31,9 @@ combos = [(a, b) for idx, a in enumerate(sequences) for b in sequences[idx + 1:]
 data = [["Start", "End", "#1", "#2", "Seq #1", "Seq #2", "Diff"]]
 for combo in combos:
     for win in windows:
-        seq_1 = combo[0].seq[int(win[1]):int(win[2])+1]
-        seq_2 = combo[1].seq[int(win[1]):int(win[2])+1]
-        data.append([win[1], win[2], combo[0].id,combo[1].id, str(seq_1), str(seq_2), round(1.0-SequenceMatcher(None, seq_1, seq_2).ratio(), 5)])
+        seq_1 = combo[0].seq[int(win[1]):int(win[2])+6+1]
+        seq_2 = combo[1].seq[int(win[1]):int(win[2])+6+1]
+        data.append([win[1], win[2]+6, combo[0].id,combo[1].id, str(seq_1), str(seq_2), round(1.0-SequenceMatcher(None, seq_1, seq_2).ratio(), 5)])
 
 with open('diff_output.tsv', 'w', newline='') as f_output:
     tsv_output = csv.writer(f_output, delimiter='\t')
